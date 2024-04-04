3226feda9000792ca1772c11d95b2639a621647a
BersainCortes <bersaincortes10@gmail.com>
2024-04-04 17:31:13 -0600

    Estilos para el NavBar

--- src/App.vue
+++ src/App.vue
@@ -16,3 +16,18 @@ export default {
 </script>
 
 <style src="./assets/bootstrap/css/bootstrap.min.css"></style>
+
+<style>
+:root{
+    --Color-1: #f7f7f7;
+    --Color-2: #2cc295;
+    --Color-2: #03624c;
+    --Color-3: #00df82;
+    --Color-4: #042222;
+    --Color-5: #021a1a;
+}
+
+.container-fluid{
+    background-color: var(--Color-1);
+}
+</style>
